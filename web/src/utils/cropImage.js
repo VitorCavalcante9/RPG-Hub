7511afacb0ce61e86064cf84975f5dfbeb57441d
Vinicius Stephanto <50119865+stephanto-dev@users.noreply.github.com>
2021-08-19 16:13:58 -0300

    Created SheetInterfaces

--- web/src/utils/cropImage.js
+++ web/src/utils/cropImage.js
@@ -1,26 +1,26 @@
-const createImage = (url) => 
+const createImage = (url) =>
   new Promise((resolve, reject) => {
-    const image = new Image()
-    image.addEventListener('load', () => resolve(image))
-    image.addEventListener('error', error => reject(error))
-    image.setAttribute('crossOrigin', 'anonymous')
-    image.src = url
-  })
+    const image = new Image();
+    image.addEventListener("load", () => resolve(image));
+    image.addEventListener("error", (error) => reject(error));
+    image.setAttribute("crossOrigin", "anonymous");
+    image.src = url;
+  });
 
 module.exports = {
   getCroppedImg: async (imageSrc, crop) => {
-    const image = await createImage(imageSrc)
-    console.log(image)
-    console.log(crop)
-    const canvas = document.createElement('canvas')
-    const ctx = canvas.getContext('2d')
+    const image = await createImage(imageSrc);
+    console.log(image);
+    console.log(crop);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
 
     /* setting canvas width & height allows us to 
     resize from the original image resolution */
     canvas.width = crop.width;
     canvas.height = crop.height;
 
-    var inputMIME = imageSrc.split(',')[0].split(':')[1].split(';')[0];
+    var inputMIME = imageSrc.split(",")[0].split(":")[1].split(";")[0];
 
     ctx.drawImage(
       image,
@@ -32,12 +32,12 @@ module.exports = {
       0,
       canvas.width,
       canvas.height
-    )
+    );
 
     return new Promise((resolve) => {
       canvas.toBlob((blob) => {
-        resolve(blob)
-      }, inputMIME)
-    })
-  }
-}
+        resolve(blob);
+      }, inputMIME);
+    });
+  },
+};
